ab5d56598fb86a9a9764a377c048cb0ad0dc05ad
semaDilthey <gaidsemen@yandex.ru>
2024-07-01 21:22:44 +0300

    added 4 different layout configurations for ComplexCell

--- WB_App/WB_App/L7_ScrollVIew/ComplexLayoutCell.swift
+++ WB_App/WB_App/L7_ScrollVIew/ComplexLayoutCell.swift
@@ -7,20 +7,72 @@
 
 import SwiftUI
 
+enum LayoutType: CaseIterable {
+    
+    case equal, mirrored, mixed, compressed
+    
+    static func random() -> Self {
+        LayoutType.allCases.randomElement()!
+    }
+    
+}
+
 struct ComplexLayoutCell: View {
+    
     var data: CellDataProvider
+    var layout : LayoutType
     
     var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                ForEach(0..<3) { index in
-                    roundedRectContent
-                }
-                .padding(.bottom, 10)
-            }
-
+        switch layout {
+        case .equal:
+            equalLayout
+        case .mirrored:
+            mirroredLayout
+        case .mixed:
+            mixedLayout
+        case .compressed:
+            compressedLayout
         }
-        .padding()
+    }
+    
+    var equalLayout: some View {
+        HStack {
+            VStack {
+                roundedRectContent
+                roundedRectContent
+            }
+            roundedRectContent
+        }
+    }
+    
+    var mirroredLayout: some View {
+        VStack {
+            roundedRectContent
+            HStack {
+                roundedRectContent
+                roundedRectContent
+            }
+        }
+    }
+    
+    var mixedLayout: some View {
+        VStack {
+            HStack {
+                roundedRectContent
+                roundedRectContent
+            }
+            roundedRectContent
+        }
+    }
+    
+    var compressedLayout: some View {
+        HStack {
+            VStack {
+                roundedRectContent
+                roundedRectContent
+            }
+            roundedRectContent
+        }
     }
 }
 
@@ -32,92 +84,9 @@ extension ComplexLayoutCell {
                 .strokeBorder(style: StrokeStyle(lineWidth: 2, dash: [5]))
                 .background(Color.clear)
             Text(data.emoji)
+                .padding()
                 .font(.largeTitle)
                 .opacity(0.5)
-        }
-    }
-}
-
-
-//struct RectanglesLayout : Layout {
-//    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
-//        <#code#>
-//    }
-//    
-//    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
-//        <#code#>
-//    }
-//    
-//
-//}
-
-struct CustomGridLayout: Layout {
-    var columns: Int
-    var spacing: CGFloat
-
-    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
-        var totalWidth: CGFloat = 0
-        var maxHeight: CGFloat = 0
-
-        var currentRowWidth: CGFloat = 0
-        var currentRowHeight: CGFloat = 0
-
-        for subview in subviews {
-            let subviewSize = subview.sizeThatFits(proposal)
-
-            if currentRowWidth + subviewSize.width + spacing > proposal.width! {
-                totalWidth = max(totalWidth, currentRowWidth)
-                maxHeight += currentRowHeight
-                currentRowWidth = 0
-                currentRowHeight = subviewSize.height
-            }
-
-            currentRowWidth += subviewSize.width + spacing
-            currentRowHeight = max(currentRowHeight, subviewSize.height)
-        }
-
-        totalWidth = max(totalWidth, currentRowWidth)
-        maxHeight += currentRowHeight
-
-        return CGSize(width: totalWidth, height: maxHeight)
-    }
-
-    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
-        var rects: [CGRect] = []
-
-        for subview in subviews {
-            let subviewSize = subview.sizeThatFits(proposal)
-
-            var rect = CGRect.zero
-
-            repeat {
-                let x = CGFloat.random(in: bounds.minX...bounds.maxX - subviewSize.width)
-                let y = CGFloat.random(in: bounds.minY...bounds.maxY - subviewSize.height)
-
-                rect = CGRect(x: x, y: y, width: subviewSize.width, height: subviewSize.height)
-            } while rects.contains(where: { $0.intersects(rect) })
-
-            rects.append(rect)
-
-            subview.place(at: rect.origin, anchor: .topLeading, proposal: proposal)
-        }
-    }
-}
-
-struct CustomGrid<Content: View>: View {
-    let columns: Int
-    let spacing: CGFloat
-    let content: () -> Content
-
-    init(columns: Int = 3, spacing: CGFloat = 10, @ViewBuilder content: @escaping () -> Content) {
-        self.columns = columns
-        self.spacing = spacing
-        self.content = content
-    }
-
-    var body: some View {
-        CustomGridLayout(columns: columns, spacing: spacing) {
-            content()
         }
     }
 }
--- WB_App/WB_App/L7_ScrollVIew/InfinityScrollView.swift
+++ WB_App/WB_App/L7_ScrollVIew/InfinityScrollView.swift
@@ -8,68 +8,54 @@
 import SwiftUI
 
 // Кастомный ScrollView с поддержкой бесконечного скролла
+// Хотелось поиграться понять как оно все тут работает при использовании дженериков
 struct InfiniteScrollView<Content: View, Data: CellDataProvider>: View {
+    
     @State private var items: [Data] = []
+    
     let content: (Data) -> Content
-    let dataForCell: () -> Data
+    let configurationModel: () -> Data
     
     private let column = [GridItem(.adaptive(minimum: .greatestFiniteMagnitude/2, 
-                                             maximum: .greatestFiniteMagnitude))
-                        ]
+                                             maximum: .greatestFiniteMagnitude))]
     
     var body: some View {
         ScrollView {
             LazyVGrid(columns: column) {
                 ForEach(items, id: \.id) { item in
                     content(item)
+                        .padding()
                         .onAppear {
-                           appendItems(item: item)
+                            withAnimation {
+                                appendItems(item: item)
+                            }
                         }
                 }
             }
-            .onAppear(perform: onAppear)
+            .onAppear(perform: {
+                withAnimation {
+                    onAppear()
+                }
+            })
         }
         .scrollIndicators(.hidden)
     }
 
     private func appendItems(item: Data) {
         if items.last?.id == item.id {
-            items.append(dataForCell())
+            items.append(configurationModel())
         }
     }
     private func onAppear() {
-        items.append(dataForCell())
+        items.append(configurationModel())
     }
 }
 
 #Preview {
     InfiniteScrollView { data in
-        ComplexLayoutCell(data: data)
-    } dataForCell: {
+        ComplexLayoutCell(data: data, layout: .random())
+    } configurationModel: {
         CellData()
     }
 
 }
-
-//
-//struct IninityCellModel {
-//    var id = UUID()
-//    var emoji : String
-//    
-//    init() {
-//        let emojiRange = 0x1F300...0x1F3F0
-//        emoji = String(UnicodeScalar(Array(emojiRange).randomElement()!)!)
-//    }
-//}
-//
-//struct ComplexLayoutCell: View {
-//    var model: IninityCellModel
-//    
-//    var body: some View {
-//        ZStack {
-//            
-//        }
-//    }
-//    
-//    
-//}
